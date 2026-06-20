5a7c21cc5580b7162f1cb61748a86cf8d9c6e318
Lee Chuk Munn <chukmunnlee@gmail.com>
2026-06-20 08:05:40 +0800

    chat client part 2, animation

--- chat-ai/src/app/chat/simple-chat.ts
+++ chat-ai/src/app/chat/simple-chat.ts
@@ -1,5 +1,5 @@
 import {NgClass} from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
@@ -23,6 +23,10 @@ export interface ChatMessage {
 })
 export class SimpleChat {
 
+  @ViewChild('#chatHistory')
+  private chatHistory!: ElementRef;
+
+  protected isLoading = false
   protected userPrompt = signal<string>('')
 
   protected readonly messages = signal<ChatMessage[]>([
@@ -32,8 +36,8 @@ export class SimpleChat {
   protected sendMessage(): void {
     const msg = this.userPrompt().trim()
     if (!!msg) {
-      console.info('send message')
       this.updateChatHistory(msg)
+      this.isLoading = true;
       this.simulateResponse()
     }
     this.userPrompt.set('')
@@ -41,12 +45,21 @@ export class SimpleChat {
 
   private updateChatHistory(msg: string, isBot = false) {
       this.messages.update(messages => [ ...messages, { text: msg, isBot } ])
+      this.scrollToBottom()
   }
 
   private simulateResponse(): void {
     setTimeout(() => {
       const resp = 'This is a simulated response'
       this.updateChatHistory(resp, true)
+      this.isLoading = false
     }, 2000)
+  }
+
+  private scrollToBottom() {
+    try {
+      this.chatHistory.nativeElement.scrollTop = this.chatHistory.nativeElement.scrollHeight
+    } catch (err) {
+    }
   }
 }
